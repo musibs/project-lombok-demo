38ef7c257c4ba19a85afa96f90c9cfb668036edf
musibs <musib.somnath@gmail.com>
2019-10-15 15:01:19 +1100

    Initial commit

---
+++ src/main/java/io/codefountain/project/lombok/Person.java
@@ -0,0 +1,14 @@
+package io.codefountain.project.lombok;
+
+
+import lombok.Getter;
+
+/**
+ * @author musib on 15/10/2019
+ * @project project-lombok-demo
+ */
+@Getter
+public class Person {
+
+
+}
